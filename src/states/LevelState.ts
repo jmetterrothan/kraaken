330b3246b8f2003b77dd242704c7a55eca13525d
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2019-04-07 14:55:38 +0200

    feat: world class

--- src/states/LevelState.ts
+++ src/states/LevelState.ts
@@ -1,46 +1,19 @@
 import { vec2 } from 'gl-matrix';
 
 import State from '@src/states/State';
-import Animation from '@src/animation/Animation';
-import Sprite from '@src/animation/Sprite';
-
-import imgAtlas32x32 from '@assets/textures/atlas32x32.png';
-
-const LOOT_CHERRY = {
-  type: "CHERRY",
-  width: 16,
-  height: 16,
-  offset: { left: 8, top: 8 },
-  animations: {
-    idle: {
-      sprite: "atlas",
-      loop:  true,
-      keyframes: [
-        { row: 1, col:  0, duration: 75 },
-        { row: 1, col:  1, duration: 75 },
-        { row: 1, col:  2, duration: 75 },
-        { row: 1, col:  3, duration: 75 },
-        { row: 1, col:  4, duration: 75 },
-        { row: 1, col:  5, duration: 75 },
-        { row: 1, col:  6, duration: 75 },
-      ]
-    }
-  }
-};
+import World from '@src/world/World';
 
 class LevelState  extends State {
-  private test: Animation;
-
+  private world: World;
+  
   constructor() {
     super();
+    this.world = new World();
   }
 
   async init() {
-    await Sprite.create(imgAtlas32x32, 'atlas', 32, 32);
-
-    this.test = new Animation('cherry_idle', LOOT_CHERRY.animations.idle);
-
     console.info('Level initialized');
+    await this.world.init();
   }
 
   mounted() {
@@ -52,27 +25,27 @@ class LevelState  extends State {
   }
 
   update(delta: number) {
-    this.test.update();
+    this.world.update(delta);
   }
 
   render(alpha: number) {
-    
+    this.world.render(alpha);
   }
 
   handleKeyboardInput(key: string, active: boolean) {
-    
+    this.world.handleKeyboardInput(key, active);
   }
 
   handleMousePressed(button: number, active: boolean, position: vec2) {
-
+    this.world.handleMousePressed(button, active, position);
   }
 
   handleMouseMove(position: vec2) {
-    
+    this.world.handleMouseMove(position);
   }
 
   handleFullscreenChange(b: boolean) {
-    
+    this.world.handleFullscreenChange(b);
   }
 }
 
--- src/world/World.ts
+++ src/world/World.ts
@@ -1,5 +1,93 @@
-class World {
+import { mat3, vec3, vec2 } from "gl-matrix";
 
+import { canvas } from "@src/Game";
+import Animation from '@src/animation/Animation';
+import Sprite from '@src/animation/Sprite';
+import Camera from '@src/Camera';
+import Box2d from "@src/Box2d";
+
+import { configSvc } from "@src/shared/services/config.service";
+
+import imgAtlas32x32 from '@assets/textures/atlas32x32.png';
+
+const LOOT_CHERRY = {
+  type: 'CHERRY',
+  width: 16,
+  height: 16,
+  offset: { left: 8, top: 8 },
+  animations: {
+      idle: {
+          sprite: 'atlas',
+          loop:  true,
+          keyframes: [
+              { row: 1, col:  0, duration: 75 },
+              { row: 1, col:  1, duration: 75 },
+              { row: 1, col:  2, duration: 75 },
+              { row: 1, col:  3, duration: 75 },
+              { row: 1, col:  4, duration: 75 },
+              { row: 1, col:  5, duration: 75 },
+              { row: 1, col:  6, duration: 75 },
+          ]
+      }
+  }
+};
+
+class World {
+  private test1: Box2d;
+  private test2: Animation;
+
+  private worldSpaceMatrix: mat3;
+  private camera: Camera;
+
+  private boundaries: Box2d;
+
+  constructor() {
+    this.worldSpaceMatrix = mat3.create();
+    this.camera = new Camera();
+  }
+
+  async init() {
+    await Sprite.create(imgAtlas32x32, 'atlas', 32, 32);
+
+    this.test1 = new Box2d(128, 128, 32, 32);
+    this.test2 = new Animation('idle', LOOT_CHERRY.animations.idle);
+
+    this.boundaries = new Box2d(0, 0, 16 * 32, 16 * 32);
+
+    this.camera.follow(this.test1);
+
+    console.info('World initialized');
+  }
+
+  update(delta: number) {
+    mat3.projection(this.worldSpaceMatrix, canvas.width, canvas.height);
+
+    this.test1.update(delta);
+    this.test2.update();
+
+    this.camera.clamp(this.boundaries);
+    this.camera.update(delta);
+  }
+
+  render(alpha: number) {
+    this.test2.render(this.worldSpaceMatrix, this.camera.getViewMatrix(), this.test1.getModelMatrix(), vec2.fromValues(1, 1));
+  }
+  
+  handleKeyboardInput(key: string, active: boolean) {
+    
+  }
+
+  handleMousePressed(button: number, active: boolean, position: vec2) {
+
+  }
+
+  handleMouseMove(position: vec2) {
+    
+  }
+  
+  handleFullscreenChange(b: boolean) {
+    this.camera.recenter();
+  }
 }
 
 export default World;
