76d6bdc79e4620dd2c249663237f07b5ee10384b
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2019-04-15 16:25:21 +0200

    feat: added react

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: {
     bundle: [
       '@babel/polyfill',
-      path.join(PATHS.SRC, 'index.ts'),
+      path.join(PATHS.SRC, 'index.tsx'),
       path.join(PATHS.SASS, 'main.scss')
     ]
   },
@@ -23,7 +23,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         enforce: 'pre',
         exclude: '/node_modules/',
         use: [
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Game from '@src/Game';
+
+import '@sass/main.scss';
+
+const game = Game.create({
+  allowFullscreen: true,
+  height: 480,
+  width: 800,
+});
+
+game.run();
+
+ReactDOM.render(<div/>, document.getElementById('root'));
