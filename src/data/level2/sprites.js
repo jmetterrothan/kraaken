6f5f4854efcecf42cd21ed745baaf3aab588241a
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2020-04-04 17:46:22 +0200

    feat: update levels WIP

--- src/data/level2/sprites.js
+++ src/data/level2/sprites.js
@@ -1,24 +1,20 @@
-import tileset from "./assets/textures/tileset.png";
-import atlas32x32 from "./assets/textures/atlas32x32.png";
-import atlas48x48 from "./assets/textures/atlas48x48.png";
-
 export default [
   {
-    src: tileset,
-    name: "tileset",
+    src: require("./assets/graphics/tileset.png").default,
+    name: "tiles",
     tileWidth: 16,
-    tileHeight: 16
+    tileHeight: 16,
   },
   {
-    src: atlas32x32,
+    src: require("./assets/graphics/atlas32x32.png").default,
     name: "atlas",
     tileWidth: 32,
-    tileHeight: 32
+    tileHeight: 32,
   },
   {
-    src: atlas48x48,
+    src: require("./assets/graphics/atlas48x48.png").default,
     name: "atlas2",
     tileWidth: 48,
-    tileHeight: 48
-  }
+    tileHeight: 48,
+  },
 ];
