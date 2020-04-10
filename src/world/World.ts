7bc365bc20604e0e3df88fe30c60352a88cfcb8c
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2020-04-10 19:34:20 +0200

    feat: bind camera to player in world init

--- src/world/World.ts
+++ src/world/World.ts
@@ -71,6 +71,7 @@ class World {
     this.initEntities(this.level.world.entities);
     this.initLoots(this.level.world.loots);
 
+    // this.camera.follow(this.player);
     this.camera.setBoundaries(this.getBoundaries());
 
     window.addEventListener("keydown", (e) => {
@@ -284,8 +285,6 @@ class World {
       this.player.showDebug();
     }
     this.add(this.player);
-
-    this.camera.follow(this.player);
   }
 
   private initEntities(entities: IObjectLevelData[]) {
