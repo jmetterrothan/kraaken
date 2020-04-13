d7cd7ac9b37540e7437a1ec55e1fdb9cf68d70e5
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2020-04-13 10:45:50 +0200

    feat: revert value changes

--- src/Game.ts
+++ src/Game.ts
@@ -38,7 +38,7 @@ class Game {
     return document.fullscreenElement !== null;
   }
 
-  public static readonly TARGET_UPS: number = 25;
+  public static readonly TARGET_UPS: number = 30;
   public static readonly MS_PER_UPDATE: number = 1000 / Game.TARGET_UPS;
   public static readonly DEFAULT_SCALE: number = 4;
 
