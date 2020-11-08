6c9617dc2da3836594628d51bd99438ac81e386c
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2020-11-08 10:40:17 +0100

    feat: improve editor ui

--- src/states/EditorState/LevelEditorUi.tsx
+++ src/states/EditorState/LevelEditorUi.tsx
@@ -7,7 +7,7 @@ import ToolbarTileset from "@src/shared/ui/components/ToolbarTileset";
 import ToolbarSeparator from "@src/shared/ui/components/ToolbarSeparator";
 import ToolbarSelect, { IToolbarOption } from "@src/shared/ui/components/ToolbarSelect";
 
-import { ILayerId } from "@src/shared/models/tilemap.model";
+import { TileLayer } from "@src/shared/models/tilemap.model";
 import { EditorMode } from "@src/shared/models/editor.model";
 import { IWorldBlueprint } from "@src/shared/models/world.model";
 
@@ -27,7 +27,7 @@ interface EditorUiProps {
     scale: number;
     mode: EditorMode;
     layerId: number;
-    tileTypeIndex: number;
+    tileTypeId: number;
   };
 }
 
@@ -40,7 +40,7 @@ const useEditorActions = () => {
       setPickMode: () => dispatch(GameEvents.modeChangeEvent(EditorMode.PICK)),
       undo: () => dispatch(GameEvents.undoEvent()),
       redo: () => dispatch(GameEvents.redoEvent()),
-      selectLayer: (option: IToolbarOption<number>) => dispatch(GameEvents.layerChangeEvent(option.value as ILayerId)),
+      selectLayer: (option: IToolbarOption<number>) => dispatch(GameEvents.layerChangeEvent(option.value as TileLayer)),
       selectTileType: (index: number) => dispatch(GameEvents.tileTypeChangeEvent(index)),
       play: (id: string) => dispatch(GameEvents.playEvent(id)),
       save: (id: string) => dispatch(GameEvents.saveEvent(id)),
@@ -53,9 +53,11 @@ const useEditorActions = () => {
 const EditorUi: React.FC<EditorUiProps> = ({ levelId, blueprint, options }) => {
   const { level, sprites } = blueprint;
 
+  const [dirty, setDirty] = React.useState(false);
+
   const [mode, setMode] = React.useState<EditorMode>(options.mode);
   const [layerId, setLayerId] = React.useState<number>(options.layerId);
-  const [tileTypeIndex, setTileTypeIndex] = React.useState<number>(options.tileTypeIndex);
+  const [tileTypeId, setTileTypeId] = React.useState<number>(options.tileTypeId);
 
   const actions = useEditorActions();
 
@@ -72,88 +74,112 @@ const EditorUi: React.FC<EditorUiProps> = ({ levelId, blueprint, options }) => {
     });
 
     const unsubFromChangeTiletypeEvent = registerEvent(GameEventTypes.CHANGE_TILETYPE_EVENT, (e: GameEvents.TileTypeChangeEvent) => {
-      setTileTypeIndex(e.detail.index);
+      setTileTypeId(e.detail.id);
     });
 
     const unsubFromChangeLayerEvent = registerEvent(GameEventTypes.CHANGE_LAYER_EVENT, (e: GameEvents.LayerChangeEvent) => {
       setLayerId(e.detail.id);
     });
 
+    const unsubFromPlaceEvent = registerEvent(GameEventTypes.PLACE_EVENT, () => {
+      setDirty(true);
+    });
+
+    const unsubFromSpawnEvent = registerEvent(GameEventTypes.SPAWN_EVENT, () => {
+      setDirty(true);
+    });
+
+    const unsubFromDespawnEvent = registerEvent(GameEventTypes.DESPAWN_EVENT, () => {
+      setDirty(true);
+    });
+
+    const unsubFromSaveEvent = registerEvent(GameEventTypes.SAVE_EVENT, () => {
+      setDirty(false);
+    });
+
     return () => {
       unsubFromChangeModeEvent();
       unsubFromChangeTiletypeEvent();
       unsubFromChangeLayerEvent();
+      unsubFromPlaceEvent();
+      unsubFromSpawnEvent();
+      unsubFromDespawnEvent();
+      unsubFromSaveEvent();
     };
   }, []);
 
   return (
     <Ui>
       <Toolbar>
-        <ToolbarButton
-          icon="play" //
-          name="Play level"
-          active={false}
-          onClick={() => actions.play(levelId)}
-        />
-        <ToolbarSeparator />
-        <ToolbarButton
-          icon="brush" //
-          name="Place"
-          active={mode === EditorMode.PLACE}
-          onClick={actions.setPlaceMode}
-        />
-        <ToolbarButton
-          icon="fill" //
-          name="Fill"
-          active={mode === EditorMode.FILL}
-          onClick={actions.setFillMode}
-        />
-        <ToolbarButton
-          icon="eraser" //
-          name="Erase"
-          theme="red"
-          active={mode === EditorMode.ERASE}
-          onClick={actions.setEraseMode}
-        />
-        <ToolbarButton
-          icon="eye-dropper" //
-          name="Pick"
-          active={mode === EditorMode.PICK}
-          onClick={actions.setPickMode}
-        />
-        <ToolbarSeparator />
-        <UndoToolbarButton onClick={actions.undo} />
-        <RedoToolbarButton onClick={actions.redo} />
-        <ToolbarSeparator />
-        <ZoomToolbarSelect onClick={actions.zoom} />
-        <ToolbarSeparator />
-        <ToolbarSelect<number>
-          icon="layer-group" //
-          selected={layerId}
-          active={false}
-          onItemClick={actions.selectLayer}
-          options={[
-            { name: "Layer 1", value: 1 },
-            { name: "Layer 2", value: 2 },
-          ]}
-        />
-        <ToolbarSeparator />
-        <ToolbarTileset
-          selected={tileTypeIndex} //
-          onSelect={actions.selectTileType}
-          src={tileSet.src}
-          tileSize={tileSet.tileWidth}
-          tileTypes={level.tileTypes}
-          tileGroups={level.tileGroups}
-          disabled={mode === EditorMode.ERASE}
-        />
-        <ToolbarSeparator />
-        <ToolbarButton
-          icon="save" //
-          name="Save"
-          active={false}
-          onClick={() => actions.save(levelId)}
-        />
+        <div>
+          <ToolbarButton
+            icon="brush" //
+            name="Place"
+            active={mode === EditorMode.PLACE}
+            onClick={actions.setPlaceMode}
+          />
+          <ToolbarButton
+            icon="fill" //
+            name="Fill"
+            active={mode === EditorMode.FILL}
+            onClick={actions.setFillMode}
+          />
+          <ToolbarButton
+            icon="eraser" //
+            name="Erase"
+            theme="red"
+            active={mode === EditorMode.ERASE}
+            onClick={actions.setEraseMode}
+          />
+          <ToolbarButton
+            icon="eye-dropper" //
+            name="Pick"
+            active={mode === EditorMode.PICK}
+            onClick={actions.setPickMode}
+          />
+          <ToolbarSeparator />
+          <UndoToolbarButton onClick={actions.undo} />
+          <RedoToolbarButton onClick={actions.redo} />
+          <ToolbarSeparator />
+          <ZoomToolbarSelect onClick={actions.zoom} />
+          <ToolbarSeparator />
+          <ToolbarSelect<number>
+            icon="layer-group" //
+            selected={layerId}
+            active={false}
+            onItemClick={actions.selectLayer}
+            options={[
+              { name: "Layer 1", value: TileLayer.L1 },
+              { name: "Layer 2", value: TileLayer.L2 },
+            ]}
+          />
+          <ToolbarSeparator />
+          <ToolbarTileset
+            selected={tileTypeId} //
+            onSelect={actions.selectTileType}
+            src={tileSet.src}
+            tileSize={tileSet.tileWidth}
+            tileTypes={level.tileTypes}
+            tileGroups={level.tileGroups}
+            disabled={mode === EditorMode.ERASE}
+          />
+        </div>
+        <div>
+          <ToolbarButton
+            icon="save" //
+            name="Save"
+            active={false}
+            disabled={!dirty}
+            onClick={() => actions.save(levelId)}
+          />
+          <ToolbarButton
+            icon="play" //
+            name="Play level"
+            active={false}
+            disabled={dirty}
+            onClick={() => actions.play(levelId)}
+          />
+        </div>
       </Toolbar>
     </Ui>
   );
