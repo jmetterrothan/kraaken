cc9cc8880fe64cb87a47e98a4a7f1aedca58ab0a
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2020-11-08 10:42:34 +0100

    feat: add local driver save/load from server

--- src/shared/drivers/AbstractDriver.ts
+++ src/shared/drivers/AbstractDriver.ts
@@ -1,9 +1,9 @@
 import { IWorldBlueprint } from '@shared/models/world.model';
-import { ILevelBlueprint } from '@shared/models/world.model';
 
 abstract class AbstractDriver {
   public abstract async load(id: string): Promise<IWorldBlueprint>;
-  public abstract async save(id: string, levelData: Partial<ILevelBlueprint>): Promise<void>;
+  public abstract async save(id: string, data: IWorldBlueprint): Promise<void>;
+
 }
 
 export default AbstractDriver;
--- src/shared/drivers/LocalDriver.ts
+++ src/shared/drivers/LocalDriver.ts
@@ -1,29 +1,39 @@
+import axios, { AxiosInstance } from 'axios';
+
 import AbstractDriver from '@src/shared/drivers/AbstractDriver';
 
+import { TileLayer } from '@shared/models/tilemap.model';
 import { IWorldBlueprint } from '@shared/models/world.model';
-import { ILevelBlueprint } from '@shared/models/world.model';
 
-class SaveError extends Error {
-  constructor (message: string) {
-    super();
-
-    this.message = message;
-    this.name = "SaveError";
-  }
-}
+import  config  from '@src/config';
 
 class LocalDriver extends AbstractDriver {
-  public async load(id: string): Promise<IWorldBlueprint> {
-    const { default: level } = await import(`@src/data/${id}/level.json`);
-    const { default: entities } = await import(`@src/data/${id}/entities.json`);
-    const { default: resources } = await import(`@src/data/${id}/resources.json`);
+  private http: AxiosInstance;
 
-    return { level, entities, sprites: resources.sprites, sounds: resources.sounds };
+  public constructor() {
+    super();
+
+    this.http = axios.create({
+      baseURL: config.LOCAL_API,
+    });
   }
 
-  public async save(id: string, levelData: Partial<ILevelBlueprint>): Promise<void> {
-    alert('[Failed] Save not yet implemented on local driver');
-    throw new SaveError('Save not implemented on local driver');
+  public async load(id: string): Promise<IWorldBlueprint> {
+    const { data: level } = await this.http.get(`/${id}/level`);
+    
+    const { default: entities } = await import(`@root/local/data/${id}/entities.json`);
+    const { default: resources } = await import(`@root/local/data/${id}/resources.json`);
+
+    return { 
+      level,
+      entities,
+      sprites: resources.sprites,
+      sounds: resources.sounds
+    };
+  }
+
+  public async save(id: string, data: IWorldBlueprint): Promise<void> {
+    await this.http.post(`/${id}/level`, data.level);
   }
 }
 
