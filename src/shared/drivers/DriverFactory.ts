4799d7dc3d3b105a3c61c9f2383be450fa92092e
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2020-11-11 20:12:42 +0100

    feat: remove driver from configSvc

--- src/shared/drivers/DriverFactory.ts
+++ src/shared/drivers/DriverFactory.ts
@@ -1,15 +1,13 @@
 import AbstractDriver from "@src/shared/drivers/AbstractDriver";
-import FirebaseDriver from "@src/shared/drivers/FirebaseDriver";
 import LocalDriver from "@src/shared/drivers/LocalDriver";
 
-export type IDriverType = 'firebase' | 'local';
+import config from '@src/config';
+
+export type IDriverType = 'local';
 
 class DriverFactory {
   static get(driver: IDriverType): AbstractDriver {
     switch(driver) {
-      case "firebase":
-        return new FirebaseDriver();
-
       case "local":
         return new LocalDriver();
 
@@ -18,5 +16,7 @@ class DriverFactory {
     }
   }
 }
+
+export const driver = DriverFactory.get(config.DRIVER);
 
 export default DriverFactory;
--- src/shared/services/ConfigService.ts
+++ src/shared/services/ConfigService.ts
@@ -1,8 +1,3 @@
-import DriverFactory from '@src/shared/drivers/DriverFactory';
-import AbstractDriver from '@src/shared/drivers/AbstractDriver';
-
-import config from '@src/config';
-
 import { IDimension } from "@shared/models/game.model";
 
 class ConfigService {
@@ -11,13 +6,9 @@ class ConfigService {
   public frameSize: IDimension;
   public innerSize: IDimension;
 
-  public driver: AbstractDriver;
-
   public constructor() {
     this.frameSize = { w: -1, h: -1 };
     this.innerSize = { w: -1, h: -1 };
-
-    this.driver = DriverFactory.get(config.DRIVER);
   }
 }
 
